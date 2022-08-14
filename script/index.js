bea884b809fec5fb37fa3387fe04182adb29e9bb
Danniel Navas <dannielnavas@gmail.com>
2022-08-14 16:50:05 -0500

    👥 if ternario

--- script/index.js
+++ script/index.js
@@ -4,17 +4,19 @@ dete = document.getElementById("age");
 button = document.getElementById("button");
 result = document.getElementById("result");
 
-function userAge (){
+function userAge() {
     let fechaActual = moment();
-    let years = fechaActual.diff(age.value, "years")
-    console.log(years)
-    if (years < 18){
-    
-        result.innerHTML = "El usuario tiene " + years + " y es menor de edad";
-    }
-    else {
-        result.innerHTML = "El usuario tiene " + years + " y es mayor de edad";
-    }
-        
-    
+    let years = fechaActual.diff(age.value, "years");
+    console.log(years);
+    // if (years < 18) {
+    //     result.innerHTML = "El usuario tiene " + years + " y es menor de edad";
+    // } else {
+    //     result.innerHTML = "El usuario tiene " + years + " y es mayor de edad";
+    // }
+
+    // if ternario
+    result.innerHTML =
+        years < 18
+            ? `El usuario tiene ${years} y es menor de edad`
+            : `El usuario tiene ${years} y es mayor de edad`;
 }
